008661f7fa02d7250cd413434fd63456206d64ad
shahabe9558 <shahabe9559@gmail.com>
2023-09-23 15:35:50 +0530

    features added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
 
-const [formdata, setFormata] = useState({firstName: "", lastName: ""});
+const [formdata, setFormata] = useState({firstName: "", lastName: "", email: "", country: "", address: "", city: "", stateProvince: "", zipCode:""});
 console.log("form dats is ", formdata);
  function changeHandler(event){
   const {name, value} = event.target;
@@ -43,6 +43,82 @@ console.log("form dats is ", formdata);
                   id='lastName'
                   className='border rounded-md border-black px-2'
                   />
+             </div> 
+             <div className='w-[88%] flex flex-col mx-auto'>
+                <label className='font-bold' htmlFor='email' >Email Address</label>
+                  <input
+                  onChange={changeHandler}
+                  type='email'
+                  placeholder='Email'
+                  name='email'
+                  value={formdata.email}
+                  id='email'
+                  className='border rounded-md border-black px-2'
+                  />
+             </div> 
+             <div className='w-[88%] flex flex-col mx-auto'>
+                <label className='font-bold' htmlFor='country' >Country</label>
+                 <select
+                 id='country'
+                 name='country'
+                 onChange={changeHandler}
+                 value={formdata.country}
+                 className='border rounded-md border-black px-2'
+                 >
+                    <option>INDIA</option>
+                    <option>Other</option>
+                 </select>
+             </div> 
+
+             <div className='w-[88%] flex flex-col mx-auto'>
+                <label className='font-bold' htmlFor='address' >Street Address</label>
+                  <textarea
+                  onChange={changeHandler}
+                  placeholder='Enter Address'
+                  name='address'
+                  value={formdata.address}
+                  id='address'
+                  className='border rounded-md border-black px-2'
+                  />
+             </div> 
+             <div className='w-[88%] flex flex-col mx-auto'>
+                <label className='font-bold' htmlFor='city' >City</label>
+                  <input
+                  onChange={changeHandler}
+                  type='text'
+                  placeholder='City'
+                  name='city'
+                  value={formdata.city}
+                  id='city'
+                  className='border rounded-md border-black px-2'
+                  />
+             </div> 
+             <div className='w-[88%] flex flex-col mx-auto'>
+                <label className='font-bold' htmlFor='statep' >State / Province</label>
+                  <input
+                  onChange={changeHandler}
+                  type='text'
+                  placeholder='State Province'
+                  name='stateProvince'
+                  value={formdata.stateProvince}
+                  id='statep'
+                  className='border rounded-md border-black px-2'
+                  />
+             </div> 
+             <div className='w-[88%] flex flex-col mx-auto'>
+                <label className='font-bold' htmlFor='zipCode' >Zip / Postal Code</label>
+                  <input
+                  onChange={changeHandler}
+                  type='number'
+                  placeholder='Zip Code'
+                  name='zipCode'
+                  value={formdata.zipCode}
+                  id='zipCode'
+                  className='border rounded-md border-black px-2'
+                  />
+             </div> 
+             <div className='w-[88%] flex flex-col mx-auto'>
+              hii
              </div> 
         </form>
     </div>
